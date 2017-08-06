e1deee387c9d7ac669b96450509d1458064899e4
Gareth Pye <gareth@cerberos.id.au>
2017-08-06 22:04:15 +1000

    At least now I can navigate, not sure why I can't do it the way I want.

--- keyboards/satan/keymaps/pyker/keymap.c
+++ keyboards/satan/keymaps/pyker/keymap.c
@@ -5,7 +5,7 @@
 
 //Tap Dance Declarations
 enum {
-  TD_E_END = 0,
+  TD_Y_END = 0,
   TD_H_HOME,
   TD_UP_MENU,
   TD_GV_ESC
@@ -14,7 +14,7 @@ enum {
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Esc, twice for Caps Lock
-  [TD_E_END]  = ACTION_TAP_DANCE_DOUBLE(KC_E, KC_END),
+  [TD_Y_END]  = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_END),
   [TD_H_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_HOME),
   [TD_UP_MENU] = ACTION_TAP_DANCE_DOUBLE(KC_UP, KC_MENU),
   [TD_GV_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
@@ -55,10 +55,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
-  TD(TD_GV_ESC),   KC_1,    KC_2,    KC_3,         KC_4, KC_5, KC_6,          KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-  TABDUAL,  KC_Q,    KC_W,    TD(TD_E_END), KC_R, KC_T, KC_Y,          KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, SLASHDUAL, \
-  CAPSDUAL, KC_A,    KC_S,    KC_D,         KC_F, KC_G, TD(TD_H_HOME), KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          ENTERDUAL,  \
-  KC_LSPO,  KC_Z,    KC_X,    KC_C,         KC_V, KC_B, KC_N,          KC_M,     KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC, \
+  TD(TD_GV_ESC),     KC_1,    KC_2,  KC_3, KC_4, KC_5,          KC_6,     KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+  TABDUAL,  KC_Q,    KC_W,    KC_E,  KC_R, KC_T, TD(TD_Y_END),  KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, SLASHDUAL, \
+  CAPSDUAL, KC_A,    KC_S,    KC_D,  KC_F, KC_G, TD(TD_H_HOME), KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          ENTERDUAL,  \
+  KC_LSPO,  KC_Z,    KC_X,    KC_C,  KC_V, KC_B, KC_N,          KC_M,     KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC, \
   KC_LCTL,  KC_LGUI, KC_LALT,                    SPACEDUAL,            RALT_T(KC_PGUP), RGUI_T(KC_PGDN), TD(TD_UP_MENU), RCTL_T(KC_DOWN)),
 
 /* Keymap _SPC: Function Layer
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_SPC] = KEYMAP_ANSI(
   KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-  KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, TO(_BL), TO(_SPC),\
+  KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
   KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT,  KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,          TO(_BL), \
   KC_TRNS, KC_TRNS, KC_APP,  KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_TAB] = KEYMAP_ANSI(
   KC_TRNS, BL_1,    BL_3,    BL_5,    BL_7,    BL_9,    BL_0   , KC_KP_EQUAL, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_TRNS, KC_TRNS, KC_TRNS,   \
-  KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS, KC_TRNS,  TO(_BL), TO(_TAB), \
+  KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS, KC_TRNS,  KC_TRNS, KC_TRNS, \
   KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ENTER, KC_TRNS,          TO(_BL), \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER,                   KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,                            KC_KP_0,                            KC_KP_00, KC_KP_DOT, KC_TRNS, KC_TRNS),
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_WORLD] = KEYMAP_ANSI(
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   \
-  TO(_TAB),KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  TO(_TAB),KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_TAB),TO(_SPC), KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          TO(_BL), \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,                            TO(_SPC),                           KC_M, KC_TRNS, KC_TRNS, KC_TRNS),
