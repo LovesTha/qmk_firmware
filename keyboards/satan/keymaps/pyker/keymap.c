e9855a07a030ed8643da5e585b4de843dbd6cf5b
Gareth Pye <gareth@cerberos.id.au>
2017-08-25 07:32:48 +1000

    Several tweaks to Pyker layout

--- keyboards/satan/keymaps/pyker/keymap.c
+++ keyboards/satan/keymaps/pyker/keymap.c
@@ -18,7 +18,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_H_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_HOME),
   [TD_UP_MENU] = ACTION_TAP_DANCE_DOUBLE(KC_UP, KC_MENU),
   [TD_GV_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
-// Other declarations would go here, separated by commas, if you have them
 };
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -36,6 +35,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define SPACEDUAL LT(_SPC, KC_SPACE)
 #define ENTERDUAL CTL_T(KC_ENT)
 #define SLASHDUAL LT(_WORLD, KC_BSLS)
+#define DWN_WKSPC LCTL(LALT(KC_DOWN))
+#define UP_WKSPC LCTL(LALT(KC_UP))
 
 #define KC_KP_00   M(0)
 
@@ -55,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
-  TD(TD_GV_ESC),     KC_1,    KC_2,  KC_3, KC_4, KC_5,          KC_6,     KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-  TABDUAL,  KC_Q,    KC_W,    KC_E,  KC_R, KC_T, TD(TD_Y_END),  KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, SLASHDUAL, \
-  CAPSDUAL, KC_A,    KC_S,    KC_D,  KC_F, KC_G, TD(TD_H_HOME), KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          ENTERDUAL,  \
-  KC_LSPO,  KC_Z,    KC_X,    KC_C,  KC_V, KC_B, KC_N,          KC_M,     KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC, \
-  KC_LCTL,  KC_LGUI, KC_LALT,                    SPACEDUAL,            RALT_T(KC_PGUP), RGUI_T(KC_PGDN), TD(TD_UP_MENU), RCTL_T(KC_DOWN)),
+  TD(TD_GV_ESC),     KC_1,    KC_2, KC_3, KC_4, KC_5,          KC_6, KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL, \
+  TABDUAL,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, TD(TD_Y_END),  KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, SLASHDUAL, \
+  CAPSDUAL, KC_A,    KC_S,    KC_D, KC_F, KC_G, TD(TD_H_HOME), KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,          ENTERDUAL,  \
+  KC_LSPO,  KC_Z,    KC_X,    KC_C, KC_V, KC_B, KC_N,          KC_M, KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC, \
+  KC_LCTL,  KC_LGUI, KC_LALT,                SPACEDUAL,            RALT_T(KC_PGUP), RGUI_T(KC_PGDN), TD(TD_UP_MENU), RCTL_T(KC_DOWN)),
 
 /* Keymap _SPC: Function Layer
    * ,-----------------------------------------------------------.
@@ -75,11 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_SPC] = KEYMAP_ANSI(
-  KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-  KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,\
-  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT,  KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,          TG(_SPC), \
-  KC_TRNS, KC_TRNS, KC_APP,  KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+  KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,   KC_BSPC,  \
+  KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_UP,   KC_TRNS, KC_TRNS, DWN_WKSPC, UP_WKSPC, KC_TRNS,\
+  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT,  KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,             TG(_SPC), \
+  KC_TRNS, KC_TRNS, KC_APP,  KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                             KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS),
 
   /* Keymap _TAB: Function Layer
    * ,-----------------------------------------------------------.
