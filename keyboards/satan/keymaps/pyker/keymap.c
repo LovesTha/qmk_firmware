8f065c2b296047dddd23ce8671b0dfe3d91ba5ad
Gareth Pye <gareth@cerberos.id.au>
2017-08-26 11:12:57 +1000

    Refinements of layout from first day at the office

--- keyboards/satan/keymaps/pyker/keymap.c
+++ keyboards/satan/keymaps/pyker/keymap.c
@@ -17,7 +17,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_Y_END]  = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_END),
   [TD_H_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_HOME),
   [TD_UP_MENU] = ACTION_TAP_DANCE_DOUBLE(KC_UP, KC_MENU),
-  [TD_GV_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
+  [TD_GV_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV)
 };
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -46,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
    * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|EnD|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
+   * |Tab  |  Q|  W|  E|  R|  T|<-Y|  U|  I|  O|  P|  [|  ]|  \  |
    * |-----------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|HoM|  J|  K|  L|  ;|  '|Return |
+   * |CAPS   |  A|  S|  D|  F|  G|->M|  J|  K|  L|  ;|  '|Return |
    * |-----------------------------------------------------------|
    * |Shift/( |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
@@ -56,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
-  TD(TD_GV_ESC),     KC_1,    KC_2, KC_3, KC_4, KC_5,          KC_6, KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL, \
+  TD(TD_GV_ESC),     KC_1,    KC_2, KC_3, KC_4, KC_5,          KC_6, KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
   TABDUAL,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, TD(TD_Y_END),  KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, SLASHDUAL, \
   CAPSDUAL, KC_A,    KC_S,    KC_D, KC_F, KC_G, TD(TD_H_HOME), KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,          ENTERDUAL,  \
   KC_LSPO,  KC_Z,    KC_X,    KC_C, KC_V, KC_B, KC_N,          KC_M, KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC, \
-  KC_LCTL,  KC_LGUI, KC_LALT,                SPACEDUAL,            RALT_T(KC_PGUP), RGUI_T(KC_PGDN), TD(TD_UP_MENU), RCTL_T(KC_DOWN)),
+  KC_LCTL,  KC_LGUI, KC_LALT,                SPACEDUAL,            RALT_T(KC_PGUP), KC_RGUI, KC_MENU, RCTL_T(KC_PGDN)),
 
 /* Keymap _SPC: Function Layer
    * ,-----------------------------------------------------------.
@@ -68,15 +68,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |     |   |Up |   |   |   |   |   |Up |   |   |   |   |     |
    * |-----------------------------------------------------------|
-   * |      |Left|Down|Rig|  |  |  |Left|Dow|Right|  |  | ToBase |
+   * |      |Left|Down|Rig|  |  |  |Left|Dow|Rgt|   |   | ToBase |
    * |-----------------------------------------------------------|
-   * |        |Tk-|Puase|TK+|V+|V- |   |   |   |   |   |         |
+   * |        |Tk-|Paus|TK+|V+|V- |   |   |   |   |   |          |
    * |-----------------------------------------------------------|
    * |    |    |    |                        |    |    |    |    |
    * `-----------------------------------------------------------'
    */
 [_SPC] = KEYMAP_ANSI(
-  KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,   KC_BSPC,  \
+  KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,   KC_DEL,  \
   KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_UP,   KC_TRNS, KC_TRNS, DWN_WKSPC, UP_WKSPC, KC_TRNS,\
   KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT,  KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,             TG(_SPC), \
   KC_TRNS, KC_TRNS, KC_APP,  KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, \
@@ -94,9 +94,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |    |    |    |                   0    | 00 | .  |    |    |
    * `-----------------------------------------------------------'
-   */
+  */
 [_TAB] = KEYMAP_ANSI(
-  KC_TRNS, BL_1,    BL_3,    BL_5,    BL_7,    BL_9,    BL_0   , KC_KP_EQUAL, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_TRNS, KC_TRNS, KC_TRNS,   \
+  KC_TRNS, BL_DEC,  BL_INC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_EQUAL, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_TRNS, KC_TRNS, KC_TRNS,   \
   KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS, KC_TRNS,  KC_TRNS, KC_TRNS, \
   KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ENTER, KC_TRNS,          TG(_TAB), \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER,                   KC_TRNS, \
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_WORLD] = KEYMAP_ANSI(
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   \
+ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   \
   TG(_TAB),KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(_TAB),TG(_SPC), KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          TG(_BL), \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, \
