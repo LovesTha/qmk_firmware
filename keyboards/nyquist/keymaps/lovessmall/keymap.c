68e9341d09393b498f5ef4480224121c694332b9
Gareth RSS Pye <Gareth@ceberos.id.au>
2018-06-07 15:12:01 +1000

    Tarak wip

--- keyboards/nyquist/keymaps/lovessmall/keymap.c
+++ keyboards/nyquist/keymaps/lovessmall/keymap.c
@@ -169,6 +169,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL, KC_LALT, KC_LGUI, EMAIL,   NUMBER,  KC_SPC,  KC_SPC,  SYMBOL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
+/* Dvorak
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Adjust| Ctrl | Alt  | GUI  |Lower |Space |Space |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_TARAK1] = KEYMAP( \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,   KC_I,    KC_SCLN, KC_P,    KC_BSPC, \
+  KC_ESC,  KC_A,    KC_O,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_S,    KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+  KC_LCTL, KC_LALT, KC_LGUI, MACRO,   NUMBER,  KC_ENT, KC_SPC, SYMBOL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+[_TARAK2] = KEYMAP( \
+ KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC, \
+ KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_F,   KC_I,    KC_SCLN, KC_P,    KC_BSPC, \
+ KC_ESC,  KC_A,    KC_O,    KC_D,    KC_U,    KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_S,    KC_QUOT, \
+ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+ KC_LCTL, KC_LALT, KC_LGUI, MACRO,   NUMBER,  KC_ENT, KC_SPC, SYMBOL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+[_TARAK3] = KEYMAP( \
+ KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC, \
+ KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_F,   KC_G,    KC_SCLN, KC_P,    KC_BSPC, \
+ KC_ESC,  KC_A,    KC_O,    KC_D,    KC_U,    KC_I,   KC_H,   KC_J,   KC_K,    KC_L,    KC_S,    KC_QUOT, \
+ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+ KC_LCTL, KC_LALT, KC_LGUI, MACRO,   NUMBER,  KC_ENT, KC_SPC, SYMBOL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+[_TARAK3] = KEYMAP( \
+ KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC, \
+ KC_TAB,  KC_Q,    KC_W,    KC_J,    KC_R,    KC_T,   KC_Y,   KC_F,   KC_G,    KC_SCLN, KC_P,    KC_BSPC, \
+ KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,   KC_D,   KC_H,   KC_K,    KC_L,    KC_S,    KC_QUOT, \
+ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+ KC_LCTL, KC_LALT, KC_LGUI, MACRO,   NUMBER,  KC_ENT, KC_SPC, SYMBOL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+[_TARAK4] = KEYMAP( \
+ KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC, \
+ KC_TAB,  KC_Q,    KC_W,    KC_J,    KC_R,    KC_T,   KC_Y,   KC_F,   KC_G,    KC_SCLN, KC_P,    KC_BSPC, \
+ KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,   KC_D,   KC_H,   KC_K,    KC_L,    KC_S,    KC_QUOT, \
+ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+ KC_LCTL, KC_LALT, KC_LGUI, MACRO,   NUMBER,  KC_ENT, KC_SPC, SYMBOL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
