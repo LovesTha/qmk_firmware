a7883cc537e33c472b8e0096af3a090034fb4471
Gareth RSS Pye <Gareth@ceberos.id.au>
2019-08-05 14:55:31 +1000

    RSS things

--- keyboards/nyquist/keymaps/lovessmall/keymap.c
+++ keyboards/nyquist/keymaps/lovessmall/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
   KC_LCTL, KC_LALT, KC_LGUI, MACRO,   NUMBER,  KC_ENT, KC_SPC, SYMBOL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______,     _______, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,          KC_0,        KC_DEL,  \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_LCBR, KC_RCBR,       KC_COLN,     _______, \
-  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,       _______,     _______, \
+  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,       _______,     KC_PIPE, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_WBAK, ALTG(KC_DOWN), ALTG(KC_UP), KC_WFWD  \
 ),
 
@@ -124,17 +124,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______,     _______, \
    KC_TILD, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9),       S(KC_0),     KC_DEL,  \
    _______, _______, _______, KC_BSLS, _______, KC_PGUP, KC_HOME, _______, KC_LBRC, KC_RBRC,       KC_COLN,     KC_DQUO, \
-   _______, _______, _______, KC_MINS, KC_EQL,  KC_PGDN, KC_END,  _______, KC_LABK, KC_RABK,       KC_QUES,     _______, \
+   _______, _______, _______, KC_MINS, KC_EQL,  KC_PGDN, KC_END,  _______, KC_LABK, KC_RABK,       KC_QUES,     KC_PIPE, \
    _______, _______, _______, _______, _______, _______, _______, _______, KC_WBAK, ALTG(KC_DOWN), ALTG(KC_UP), KC_WFWD  \
 ),
 
 /* Macro layer */
 [_MACRO] = KEYMAP( \
-   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-   TICCTX , TICCRX , EMAIL,   TRELOAD, TREB00T, NEWTAB,  _______, _______, _______, _______, TRAINZ,  _______, \
-   TICCISR, K4321  , K1441  , TFTS,    FINDALL, ASSRT  , TRICCM,  _______, _______, PLESS  , _______, _______, \
-   _______, TCPU   , DETECT , REPLACE, REMISR,  PROGRM,  TRICCS,  _______, _______, _______, _______, _______, \
-   ADJUST , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+   _______, _______, EMAIL,   _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   TICCTX , KC_1,    KC_2,    KC_3,    TREB00T, NEWTAB,  _______, _______, _______, _______, TRAINZ,  _______, \
+   TICCISR, KC_4,    KC_5,    KC_6,    FINDALL, ASSRT,   TRICCM,  _______, K1441,   PLESS  , _______, _______, \
+
+   _______, KC_7,    KC_8,    KC_9,    REMISR,  PROGRM,  TRICCS,  _______, _______, _______, _______, _______, \
+   ADJUST , _______, KC_0,    _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
 /* Colemak
