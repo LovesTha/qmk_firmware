9ea803c6b0e41bdfce6f66488ed9afc99a5be053
Gareth Pye <gareth@cerberos.id.au>
2020-10-21 15:06:03 +1100

    Working but only 4 columns scan

---
+++ keyboards/ltstagger/config.h
@@ -0,0 +1,74 @@
+/*
+Copyright 2017 Danny Nguyen <danny@hexwire.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x1234
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    LovesKeebs
+#define PRODUCT         The LTStagger Keyboard
+#define DESCRIPTION     Split 40 percent columnar stagger keyboard
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
+
+// wiring of each half
+#define MATRIX_ROW_PINS { F6, F7, B1, B3, B4, B5 }
+#define MATRIX_COL_PINS { D4, C6, D7, E6 }
+//#define SPLIT_HAND_PIN D5
+#define EE_HANDS
+#define SPLIT_USB_DETECT
+#define USE_I2C
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+#define DEBUG_MATRIX_SCAN_RATE
+
+/* define if matrix has ghost */
+//#define MATRIX_HAS_GHOST
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+// #define NO_DEBUG
+
+/* disable print */
+// #define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
