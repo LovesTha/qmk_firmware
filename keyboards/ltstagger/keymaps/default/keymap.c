c150fdbd3ad51ca076c4782e9f2bb9295594dfd5
Gareth Pye <gareth.pye@exteltechnologies.com>
2020-11-16 13:11:29 +1100

    Better layout

--- keyboards/ltstagger/keymaps/default/keymap.c
+++ keyboards/ltstagger/keymaps/default/keymap.c
@@ -32,10 +32,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-      KC_TAB,  KC_Q, KC_W, KC_E,   KC_R,   KC_T,     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_ESC,  KC_A, KC_S, KC_D,   KC_F,   KC_G,     KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, MT(MOD_LCTL, KC_Z), MT(MOD_LALT, KC_X), KC_C,   KC_V,   KC_B,     KC_N,   KC_M,    KC_COMM, MT(MOD_RALT, KC_DOT),  MT(MOD_RCTL, KC_SLSH), MT(MOD_RSFT, KC_BSLASH),
-      _______,_______,_______, MO(_MACRO), MO(_NUMBER),  LCTL_T(KC_ENT), RCTL_T(KC_SPC), MO(_SYMBOL), MO(_MACRO), _______,_______,_______
+      LALT_T(KC_TAB),  KC_Q, KC_W, KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    RALT_T(KC_BSPC),
+      LSFT_T(KC_ESC),  KC_A, KC_S, KC_D,   KC_F,   KC_G,               KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, RSFT_T(KC_QUOT),
+      MOD_LCTL,        KC_Z, KC_X, KC_C,   KC_V,   KC_B,               KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, RCTL_T(KC_BSLASH),
+      _______,_______,_______, MO(_MACRO), KC_ENT, MO(_NUMBER), MO(_SYMBOL), KC_SPC, MO(_MACRO), _______,_______,_______
     ),
     [_NUMBER] = LAYOUT( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,          KC_0,        KC_DEL,  \
